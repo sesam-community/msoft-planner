c29fc1ca658252893f059cb7c26164dfc191a9ee
HakonMellbye <31470612+HakonMellbye@users.noreply.github.com>
2019-12-12 09:41:37 +0100

    prepared for integration with sesame

--- service/plans_nd_tasks.py
+++ service/plans_nd_tasks.py
@@ -1,6 +1,7 @@
 import requests
-
+import logging
 from dao_helper import get_all_objects, get_object,make_request,GRAPH_URL
+
 RESOURCE_PATH = '/planner/tasks'
 
 def get_plans(group_generator_func):
@@ -28,23 +29,6 @@ def get_plans_for_group(group_id):
         # already logged in make_request function, no action needed
         pass
 
-def create_tasks():
-    #   "planId": "Py6LMdklhES5PgCK4xyUcpcAHrQT",
-    #   "bucketId": "21xu56a4ykSdmaVjflkl75cAOedV",
-    test_task={"planId":"Py6LMdklhES5PgCK4xyUcpcAHrQT","title":"task from func2","bucketId":"21xu56a4ykSdmaVjflkl75cAOedV"}
-    make_request(f'{GRAPH_URL}{RESOURCE_PATH}', 'POST', test_task)
-    print("created task")
-
-def update_tasks():
-    taskId = "/ljJLCYPkXk2Muq3mEV8DD5cAANBH"
-    etag= 'W/"JzEtVGFzayAgQEBAQEBAQEBAQEBAQEBARCc="'
-    bucket = "21xu56a4ykSdmaVjflkl75cAOedV",
-    print("try to update the created task")
-    update_task_data={"title":"updated title2","percentComplete":95,"@odata.etag":etag, "bucketId":"21xu56a4ykSdmaVjflkl75cAOedV"}
-    print("requeset made: \t",update_task_data)
-    make_request(f'{GRAPH_URL}{RESOURCE_PATH}{taskId}', 'PATCH',update_task_data)
-    print("sucsessfully updated task")
-
 
 def get_plan_details(plan_id):
     return get_object(f'/planner/plans/{plan_id}/details')
@@ -52,3 +36,39 @@ def get_plan_details(plan_id):
 
 def get_task_details(task_id):
     return get_object(f'/planner/tasks/{task_id}/details')
+
+def create_tasks(task_data):
+    """
+    Function to create a task
+    :param task_data: json object with group details, must contain plan identifier
+    :param task_data: {title:string, percentComplete:int, dueDate:dateTimeTimeZone, assigneePriority: string, bucketId:string}
+    :return: void
+    """
+    plan_id = task_data['plan_id'] if 'plan_id' in plan_data else None
+
+    if not plan_id:
+        raise Exception("Couldn't find id for plan")
+
+    logging.info(f'trying to create task {task_data.get("title")}')
+    make_request(f'{GRAPH_URL}{RESOURCE_PATH}', 'POST', task_data)
+    logging.info(f'group {group_data.get("displayName")} updated successfully')
+
+
+
+def update_tasks(task_data):
+    """
+    :param task_id:
+    :param task_update_data: {@odata.etag:string, title:string, percentComplete:int, dueDate:dateTimeTimeZone, assigneePriority: string, bucketId:string}
+    :return:
+    """
+
+    task_id = task_data['task_id'] if 'task_id' in task_data else None
+
+
+    if not task_id:
+        raise Exception("Couldn't find id for plan")
+        #task_data = {"title":"updated title2","percentComplete":50,"@odata.etag":'W/"JzEtVGFzayAgQEBAQEBAQEBAQEBAQEBARCc="', "bucketId":"21xu56a4ykSdmaVjflkl75cAOedV"}
+
+
+    make_request(f'{GRAPH_URL}{RESOURCE_PATH}{task_id}', 'PATCH',task_data)
+    print("sucsessfully updated task")
