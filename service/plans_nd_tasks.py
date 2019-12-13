107b29f6369d04498d37f7f91ac10af595c0fecc
HakonMellbye <31470612+HakonMellbye@users.noreply.github.com>
2019-12-13 11:47:49 +0100

    add extra loop

--- service/plans_nd_tasks.py
+++ service/plans_nd_tasks.py
@@ -47,7 +47,15 @@ def create_tasks(task_data_generator):
     """
     for task_data in task_data_generator:
         plan_id = task_data.get("planId") if 'planId' in task_data else None
-        logging.error("gen: task_data type = " +str(type(task_data)))
+        logging.info("planId = " +planId)
+        logging.error("gen: task_data= "+ task_data +"task_data type = " +str(type(task_data)))
+        try:
+            for data in task_data:
+                plan_id = data.get("planId")
+                logging.info("planId = " +planId)
+                logging.error("gen: task_data= "+ task_data +"task_data type = " +str(type(task_data)))
+        except:
+            logging.error("extra loop failed: data = " + data)
         if not plan_id:
             try:
                 logging.error("planId not found, task data= " + json.dumps(task_data)+" task_data type = " +str(type(task_data)))
