9123a8a64114cadb3a7aa339c7228953e95f6d85
Axel Borge <axel.borge@gmail.com>
2022-05-12 12:48:49 +0200

    Fix issues on get Post

--- service/plans_nd_tasks.py
+++ service/plans_nd_tasks.py
@@ -1,4 +1,7 @@
 import requests
+
+
+from dao_helper import get_all_objects, get_object
 import logging
 import json
 from dao_helper import get_all_objects, get_object,make_request,GRAPH_URL
@@ -8,12 +11,34 @@ RESOURCE_PATH = "/planner/tasks/"
 logging = logging.getLogger('microsoftplanner-connector.plans_nd_tasks')
 
 
+
 def get_plans(group_generator_func):
     for group in group_generator_func:
         for plan in get_plans_for_group(group['id']):
-            plan['details'] = get_plan_details(plan['id'])
+            plan['details'] = get_plan_details(group['id'], plan['id'])
+            plan['group_id'] = group['id']
             yield (plan)
 
+def get_conversations(group_generator_func):
+    for group in group_generator_func:
+        for conversation in get_conversations_for_group(group['id']):
+            conversation['details'] = get_conversation_details(group['id'], conversation['id'])
+            conversation['group_id'] = group['id']
+            yield (conversation)
+
+
+def get_threads(conversation_generator_func):
+    for conversation in conversation_generator_func:
+        for task in get_threads_for_conversation(conversation['group_id'],conversation['id']):
+            task['details'] = get_thread_details(conversation['group_id'],conversation['id'],task['id'])
+            task['group_id'] = conversation['group_id']
+            yield task
+
+def get_posts(conversation_generator_func):
+    for thread in conversation_generator_func:
+        for post in get_posts_for_thread(thread['group_id'],thread['id']):
+            post['details'] = get_post_details(thread['group_id'],thread['id'],post['id'])
+            yield post
 
 def get_tasks(plan_generator_func):
     for plan in plan_generator_func:
@@ -21,25 +46,55 @@ def get_tasks(plan_generator_func):
             task['details'] = get_task_details(task['id'])
             yield task
 
-
 def get_tasks_for_plan(plan_id):
     yield from get_all_objects(f'/planner/plans/{plan_id}/tasks')
-
 
 def get_plans_for_group(group_id):
     try:
         yield from get_all_objects(f'/groups/{group_id}/planner/plans')
     except requests.exceptions.HTTPError:
-        # already   logged in make_request function, no action needed
+        # already logged in make_request function, no action needed
+        pass
+
+def get_conversations_for_group(group_id):
+    try:
+        yield from get_all_objects(f'/groups/{group_id}/conversations')
+    except requests.exceptions.HTTPError:
+        # already logged in make_request function, no action needed
+        pass
+
+def get_threads_for_conversation(group_id, conversation_id):
+    try:
+        yield from get_all_objects(f'/groups/{group_id}/conversations/{conversation_id}/threads')
+    except requests.exceptions.HTTPError:
+        # already logged in make_request function, no action needed
+        pass
+
+def get_posts_for_thread(group_id, thread):
+    try:
+        yield from get_all_objects(f'/groups/{group_id}/threads/{thread}/posts')
+    except requests.exceptions.HTTPError:
+        # already logged in make_request function, no action needed
         pass
 
 
-def get_plan_details(plan_id):
-    return get_object(f'/planner/plans/{plan_id}/details')
+def get_plan_details(group_id, plan_id):
+    return get_object(f'/groups/{group_id}/planner/plans/{plan_id}/details')
 
+def get_conversation_details(group_id, id):
+    return get_object(f'/groups/{group_id}/conversations/{id}')
+
+def get_post_details(group_id, thread, id):
+    return get_object(f'/groups/{group_id}/threads/{thread}/posts/{id}')
+
+def get_thread_details(group_id, conversation_id, thread_id):
+    return get_object(f'/groups/{group_id}/conversations/{conversation_id}/threads/{thread_id}')
 
 def get_task_details(task_id):
-    return get_object(f'/planner/tasks/{task_id}/details')
+    try:
+        return get_object(f'/planner/tasks/{task_id}/details')
+    except Exception as e:
+        return "Access denied..."
 
 def create_plans(plan_data :list):
     CP = CreatePlannerPlans()
--- service/service.py
+++ service/service.py
@@ -11,14 +11,15 @@ import os
 from logger_helper import log_request
 from auth_helper import add_token_to_cache, get_tokens_as_app, sign_in_redirect_as_app
 from dao_helper import init_dao, get_all_objects, stream_as_json
-from plans_nd_tasks import get_plans, get_tasks, create_tasks,update_tasks, create_plans
+from plans_nd_tasks import get_plans, get_tasks, get_conversations, get_threads, get_posts, create_tasks,update_tasks, create_plans
 from planner_groups import get_all_groups
 from planner_users import get_all_users
-from planner_buckets import create_buckets
+from planner_buckets import get_all_buckets, get_buckets, create_buckets
 
 app = Flask(__name__)
 
 env = os.environ.get
+
 client_id = env('client_id')
 client_secret = env('client_secret')
 tenant_id = env('tenant_id')
@@ -32,7 +33,7 @@ user_code_info = None
 app.secret_key = uuid.uuid4().bytes
 
 required_env_vars = ['client_id', 'client_secret', 'tenant_id']
-missing_env_vars = list()
+missing_env_vars = list() 
 
 ## Helper functions
 def check_env_variables(required_env_vars, missing_env_vars):
@@ -40,7 +41,7 @@ def check_env_variables(required_env_vars, missing_env_vars):
         value = os.getenv(env_var)
         if not value:
             missing_env_vars.append(env_var)
-
+        
     if len(missing_env_vars) != 0:
         app.logger.error(f"Missing the following required environment variable(s) {missing_env_vars}")
         sys.exit(1)
@@ -52,20 +53,12 @@ def index():
     if user_code_info is None:
         user_code_info = sign_in_redirect_as_app(client_id, tenant_id)
 
-    output = (f"{user_code_info.get('message')}")
-    url = output[47:80]
-    code = output[100:109]
+        output = (f"{user_code_info.get('message')}")
+        url = output[47:80]
+        code = output[100:109]
 
-    return render_template('index.html', url=url, code=code)
+        return render_template('index.html', url=url, code=code)
 
-
-@app.route('/auth', methods=['POST', 'GET'])
-@log_request
-def auth_user():
-    """
-    Endpoint to sign in user interactively by using Microsoft login page
-    :return:
-    """
     global token
     app.logger.info("Microsoft Planner Service running on /auth port as expected")
     try:
@@ -75,7 +68,7 @@ def auth_user():
             request_count = 1
         if 'access_token' in token:
             app.logger.info('Adding access token to cache...')
-            (client_id, tenant_id, token)
+            add_token_to_cache(client_id, tenant_id, token)
             return_object = (f"{token['refresh_token']}")
             return render_template('token.html', return_object=return_object)
         else:
@@ -88,24 +81,62 @@ def auth_user():
         return_error = ("You're logged in with the wrong user. Please log out and authenticate again.")
         return render_template('token.html', return_error=return_error)
 
-@app.route('/planner/<var>', methods=['GET', 'POST','PATCH'])
+
+@app.route('/auth', methods=['POST', 'GET','PATCH'])
+@log_request
+def auth_user():
+    """
+    Endpoint to sign in user interactively by using Microsoft login page
+    :return:
+    """
+    global token
+    app.logger.info("Microsoft Planner Service running on /auth port as expected")
+    try:
+        request_count = 0
+        if request_count == 0:
+            token = get_tokens_as_app(client_id, user_code_info, tenant_id)
+            request_count = 1  
+        if 'access_token' in token:
+            app.logger.info('Adding access token to cache...')
+            add_token_to_cache(client_id, tenant_id, token)
+            return_object = (f"{token['refresh_token']}")
+            return render_template('token.html', return_object=return_object)
+        else:
+            return_error = ("Token response did not result in a proper response. Athenticate again please.")
+            return render_template('token.html', return_error=return_error)
+    except AttributeError or TypeError:
+        return_error = ('Authentification failed. Please pull and restart your system and authenticate again.')
+        return render_template('token.html', return_error=return_error)
+    except adal.AdalError as err:
+        return_error = ("You're logged in with the wrong user. Please log out and authenticate again.")
+        return render_template('token.html', return_error=return_error)
+
+@app.route('/planner/<var>', methods=['GET', 'POST'])
 @log_request
 def list_all_tasks(var):
     """
     Endpoint for calling Graph API
     """
-    init_dao(client_id, client_secret, tenant_id, refresh_token)
-
+    init_dao(client_id, client_secret, tenant_id, refresh_token) 
+    
     if var.lower() == "tasks":
         app.logger.info(f'Requesting {var} from the graph API')
         return Response(stream_as_json(get_tasks(get_plans(get_all_objects('/groups/')))), content_type='application/json')
     elif var.lower() == "plans":
         app.logger.info(f'Requesting {var} from the graph API')
         return Response(stream_as_json(get_plans(get_all_objects('/groups/'))), request.args.get('since'), content_type='application/json')
-    elif var.lower() == "create_plans":
+    elif var.lower() == "conversations":
         app.logger.info(f'Requesting {var} from the graph API')
-        request_data = request.get_data()
-        return create_plans(json.loads(str(request_data.decode("utf-8"))))
+        return Response(stream_as_json(get_conversations(get_all_objects('/groups/'))), request.args.get('since'), content_type='application/json')
+    elif var.lower() == "threads":
+        app.logger.info(f'Requesting {var} from the graph API')
+        return Response(stream_as_json(get_threads(get_conversations(get_all_objects('/groups/')))), request.args.get('since'),
+                        content_type='application/json')
+    elif var.lower() == "posts":
+        app.logger.info(f'Requesting {var} from the graph API')
+        return Response(stream_as_json(get_posts(get_threads(get_conversations(get_all_objects('/groups/'))))),
+                        request.args.get('since'),
+                        content_type='application/json')
     elif var.lower() == "groups":
         app.logger.info(f'Requesting {var} from the graph API')
         return Response(get_all_groups(request.args.get('since')), content_type='application/json')
@@ -122,10 +153,9 @@ def list_all_tasks(var):
         return update_tasks(json.loads(str(request_data.decode("utf-8"))))
     elif var.lower() == "create_buckets":
         app.logger.info(f'Requesting {var} from the graph API')
+    elif var.lower() == "create_plans":
         request_data = request.get_data()
-        return create_buckets(json.loads(str(request_data.decode("utf-8"))))
-
-
+        return create_plans(json.loads(str(request_data.decode("utf-8"))))
     else:
         app.logger.warning(f'The following request value : {var} \n - does not comply with what is currently configured backend')
         return Response(json.dumps({"You need to choose a configured <value> in the path '/planner/<value>'" : "I.e. : 'tasks', 'plans', 'groups' or 'users'"}), content_type='application/json')
